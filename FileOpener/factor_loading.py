1eb1395584161c3234de5c60ba924e2f1500ba97
unknown <mannypac12@gmail.com>
2019-02-12 20:27:07 +0900

    Factor Loading Added

---
+++ FileOpener/factor_loading.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+
+
+"""
+1. 팩터에 필요한 Component 를 만드는 함수입니닷 뀨
+"""
+
+## Factor Creator
+class Factor:
+
+    ## 우량성 계산
+        ## 각 항목별 순위를 매긴 후
+        ## 매겨진 순위를 바탕으로 각 종목별 z-score를 계산한다.
+        ## 지표 내 모든 항목의 z-score를 합한 후
+        ## 합한 값을 바탕으로 z-score를 산출함
+
+    class size:
+
+        def __init__(self, marketcap):
+
+            pass
+
+    class Value:
+
+        def __init__(self, price, pershare):
+
+            ## Price: 가격 데이터
+            ## perShare: 주당 팩터(EPS / SPS / CPS 등)
+
+            self.price = price
+            self.pershare = pershare
+
+        def value_fact_comp(self, st_year=2002, ed_year=2019, non_zero=True):
+
+            dt = pd.DataFrame()
+
+            for year in range(st_year, ed_year):
+
+                dt = pd.concat([dt, self.price.loc[f"{year}-01-01":f"{year}-03-31"].div(self.pershare.loc[f"{year-2}-12-31"])])
+                dt = pd.concat([dt, self.price.loc[f"{year}-04-01":f"{year}-12-31"].div(self.pershare.loc[f"{year-1}-12-31"])])
+
+            if non_zero == True:
+
+                dt[dt < 0] = np.nan
+
+                return dt
+
+            elif non_zero == False:
+
+                return dt
+
+    class Momentum:
+
+        def __init__(self, price, pershare):
